beb8c3676eb344bfe45d5914f1102b1c4b816f77
KaleeBurchett <kburchett11@ivytech.edu>
2025-07-23 16:34:19 -0400

    Unique usernames and register redirect

--- model/User.js
+++ model/User.js
@@ -4,7 +4,8 @@ const passportLocalMongoose = require('passport-local-mongoose');
 var User = new Schema({
     username: {
         type: String,
-        required : true
+        required : true,
+        unique : true
     },
     password: {
         type: String,
